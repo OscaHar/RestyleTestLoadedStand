1715d9e68d828ddda7792a35bee16abd62034326
Max Runesson <Max@maxRunesson.se>
2021-02-23 09:48:09 +0100

    Added position, rotation and scail

--- public/client.js
+++ public/client.js
@@ -3,6 +3,8 @@ import {GLTFLoader} from '/jsm/loaders/GLTFLoader.js';
 
 var scene, camera, renderer;
 
+var group;
+
 var init = function(){
 
     scene = new THREE.Scene();
@@ -10,7 +12,7 @@ var init = function(){
     camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
     camera.position.x = -10;
     camera.position.y = -10;
-    camera.position.z = 20;
+    camera.position.z = 20; 
 
     var hlight = new THREE.AmbientLight (0x404040,100);
     scene.add(hlight);
@@ -21,7 +23,14 @@ var init = function(){
 
     var loader = new GLTFLoader();
     loader.load('scene.gltf', function(gltf){
-        scene.add(gltf.scene);
+        //camera position z might need to be moved back (To about 200) depending on settings with object.
+        group = gltf.scene;
+        group.position.x = 0;
+        group.position.y = 0;
+        group.rotation.set(0, 0, 0);
+        group.scale.set(1, 1, 1);
+
+        scene.add(group);
         renderer.render(scene,camera);
     });
 };
