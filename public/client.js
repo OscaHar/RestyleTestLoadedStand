b00743ad20dfa417386cfe49952e95e92c625cc7
Max Runesson <Max@maxRunesson.se>
2021-02-23 09:53:03 +0100

    Made code easier to read and removed unessesery code

--- public/client.js
+++ public/client.js
@@ -9,29 +9,28 @@ var init = function(){
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
+    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 5000 );
     camera.position.x = -10;
     camera.position.y = -10;
     camera.position.z = 20; 
 
-    var hlight = new THREE.AmbientLight (0x404040,100);
-    scene.add(hlight);
+    var hlight = new THREE.AmbientLight ( 0x404040, 100 );
+    scene.add( hlight );
 
-    renderer = new THREE.WebGLRenderer({antialias:true});
-    renderer.setSize(window.innerWidth,window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    renderer = new THREE.WebGLRenderer( {antialias:true} );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
     var loader = new GLTFLoader();
-    loader.load('scene.gltf', function(gltf){
-        //camera position z might need to be moved back (To about 200) depending on settings with object.
+    loader.load( 'scene.gltf', function( gltf ){
+        //camera position z might need to be moved back to about 200 just to see the object.
         group = gltf.scene;
-        group.position.x = 0;
-        group.position.y = 0;
-        group.rotation.set(0, 0, 0);
-        group.scale.set(1, 1, 1);
+        group.position.set ( 0, 0, 0 );
+        group.rotation.set( 0, 0, 0 );
+        group.scale.set( 1, 1, 1 );
 
-        scene.add(group);
-        renderer.render(scene,camera);
+        scene.add( group );
+        renderer.render( scene, camera );
     });
 };
 init();
