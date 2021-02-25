14438017a8902ccd6472eeb2591410c08ffaa4c6
Oscar Harrysson <harrysson.oscar@gmail.com>
2021-02-25 14:54:51 +0100

    Initial commit

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require("express");
+const app = express();
+const path = require("path");
 
-app.use(express.static(__dirname + '/public'))
-app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
-app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
-
-app.listen(3000, () =>
-  console.log('hej')
+app.use(express.static(__dirname + "/public"));
+app.use(
+  "/build/",
+  express.static(path.join(__dirname, "node_modules/three/build"))
 );
+app.use(
+  "/jsm/",
+  express.static(path.join(__dirname, "node_modules/three/examples/jsm"))
+);
+
+app.listen(8000, () => console.log("hej"));
--- public/client.js
+++ public/client.js
@@ -1,42 +1,46 @@
-import * as THREE from '/build/three.module.js';
-import {GLTFLoader} from '/jsm/loaders/GLTFLoader.js';
+import * as THREE from "/build/three.module.js";
+import { GLTFLoader } from "/jsm/loaders/GLTFLoader.js";
 
 var scene, camera, renderer;
 
 var group1, group2;
 
-var scenePath1 = '/Stands/Stand1/scene.gltf';
-var scenePath2 = '/Stands/Stand2/scene.gltf';
+var scenePath1 = "/Stands/Stand1/scene.gltf";
+//var scenePath2 = '/Stands/Stand2/scene.gltf';
 
-var init = function(){
+var init = function () {
+  scene = new THREE.Scene();
 
-    scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    40,
+    window.innerWidth / window.innerHeight,
+    1,
+    5000
+  );
+  camera.position.x = -10;
+  camera.position.y = -10;
+  camera.position.z = 200;
 
-    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 5000 );
-    camera.position.x = -10;
-    camera.position.y = -10;
-    camera.position.z = 200; 
+  var hlight = new THREE.AmbientLight(0x404040, 100);
+  scene.add(hlight);
 
-    var hlight = new THREE.AmbientLight ( 0x404040, 100 );
-    scene.add( hlight );
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-    renderer = new THREE.WebGLRenderer( {antialias:true} );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+  var loader = new GLTFLoader();
+  loader.load(scenePath1, function (gltf) {
+    //camera position z might need to be moved back to about 200 just to see the object.
+    group1 = gltf.scene;
+    group1.position.set(-10, -55, 0);
+    group1.rotation.set(0, 0, 0);
+    group1.scale.set(0.25, 0.25, 0.25);
 
-    var loader = new GLTFLoader();
-    loader.load( scenePath1, function( gltf ){
-        //camera position z might need to be moved back to about 200 just to see the object.
-        group1 = gltf.scene;
-        group1.position.set ( 50, -50, 0 );
-        group1.rotation.set( 0, 0, 0 );
-        group1.scale.set( .1, .1, .1 );
+    scene.add(group1);
+    renderer.render(scene, camera);
+  });
 
-        scene.add( group1 );
-        renderer.render( scene, camera );
-    });
-
-    loader.load( scenePath2, function( gltf ){
+  /*     loader.load( scenePath2, function( gltf ){
         //camera position z might need to be moved back to about 200 just to see the object.
         group2 = gltf.scene;
         group2.position.set ( -50, -50, 0 );
@@ -45,6 +49,6 @@ var init = function(){
 
         scene.add( group2 );
         renderer.render( scene, camera );
-    });
+    }); */
 };
 init();
