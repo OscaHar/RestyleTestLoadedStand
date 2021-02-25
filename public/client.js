ff1c74c51ed6c95ee88432f62c41c6f33a9389a8
Max Runesson <Max@maxRunesson.se>
2021-02-25 08:51:43 +0100

    Loading stands

--- public/client.js
+++ public/client.js
@@ -3,7 +3,10 @@ import {GLTFLoader} from '/jsm/loaders/GLTFLoader.js';
 
 var scene, camera, renderer;
 
-var group;
+var group1, group2;
+
+var scenePath1 = '/Stands/Stand1/scene.gltf';
+var scenePath2 = '/Stands/Stand2/scene.gltf';
 
 var init = function(){
 
@@ -12,7 +15,7 @@ var init = function(){
     camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 5000 );
     camera.position.x = -10;
     camera.position.y = -10;
-    camera.position.z = 20; 
+    camera.position.z = 200; 
 
     var hlight = new THREE.AmbientLight ( 0x404040, 100 );
     scene.add( hlight );
@@ -22,14 +25,25 @@ var init = function(){
     document.body.appendChild( renderer.domElement );
 
     var loader = new GLTFLoader();
-    loader.load( 'scene.gltf', function( gltf ){
+    loader.load( scenePath1, function( gltf ){
         //camera position z might need to be moved back to about 200 just to see the object.
-        group = gltf.scene;
-        group.position.set ( 0, 0, 0 );
-        group.rotation.set( 0, 0, 0 );
-        group.scale.set( 1, 1, 1 );
+        group1 = gltf.scene;
+        group1.position.set ( 50, -50, 0 );
+        group1.rotation.set( 0, 0, 0 );
+        group1.scale.set( .1, .1, .1 );
 
-        scene.add( group );
+        scene.add( group1 );
+        renderer.render( scene, camera );
+    });
+
+    loader.load( scenePath2, function( gltf ){
+        //camera position z might need to be moved back to about 200 just to see the object.
+        group2 = gltf.scene;
+        group2.position.set ( -50, -50, 0 );
+        group2.rotation.y = (Math.PI/2);
+        group2.scale.set( .1, .1, .1 );
+
+        scene.add( group2 );
         renderer.render( scene, camera );
     });
 };
